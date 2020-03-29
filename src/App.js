d6de9e941f8831f4846c9fc13dd1e3c237662f59
George Tzinetas <krimini.kriminals@gmail.com>
2020-03-30 01:02:36 +0300

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +15,25 @@ function App() {
         >
           Learn React
         </a>
+        <Example />
       </header>
     </div>
   );
 }
-
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(0)}>Reset me</button>
+    </div>
+  );
+}
 export default App;
